4c7cb98f1bae34507a2d528a0dd41198357bd8ad
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-06 06:50:09 +0900

    단순계산 구현(곱셈/나눗셈 우선 계산X)

--- src/index.js
+++ src/index.js
@@ -21,10 +21,33 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
+const operatorFunctions = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y
+}
+
+function calculater({ numberStack, accStack }) {
+  console.log("받은 것 numberStack : ", numberStack)
+  console.log("받은 것 accStack : ", accStack)
+  if (numberStack.length === 1) {
+    return numberStack[0];
+  } else {
+    for (i = 0; i < accStack.length; i++) {
+      numberStack.splice(0, 2, operatorFunctions[accStack[i]](numberStack[i], numberStack[i + 1]));
+      accStack.splice(0, 1)
+      return calculater({ numberStack: numberStack, accStack: accStack })
+    }
+  }
+}
+
 function render({ tempNumber, numberStack, accStack }) {
   console.log(tempNumber)
   console.log("숫자 배열", numberStack);
   console.log("연산자 배열", accStack)
+
+
 
   function handleClickNumber(e) {
     render({
@@ -35,13 +58,24 @@ function render({ tempNumber, numberStack, accStack }) {
   }
 
   function handleClickAcc(e) {
+    //연산자 연속 2번 입력 막는 거 구현해보기
     render({
       tempNumber: 0,
       numberStack: [...numberStack, tempNumber],
       accStack: [...accStack, e.target.innerText]
     })
   }
-
+  function handleClickResult() {
+    numberStack = [...numberStack, tempNumber];
+    render({
+      tempNumber: calculater({
+        numberStack: numberStack,
+        accStack: accStack
+      }),
+      numberStack: [],
+      accStack: []
+    })
+  }
   function buttonMaker(arr, event) {
     return (
       <p>
@@ -61,7 +95,8 @@ function render({ tempNumber, numberStack, accStack }) {
       </div>
       <div>
         {buttonMaker([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], handleClickNumber)}
-        {buttonMaker(['+', '-', '*', '/', '='], handleClickAcc)}
+        {buttonMaker(['+', '-', '*', '/'], handleClickAcc)}
+        {buttonMaker(['='], handleClickResult)}
       </div>
     </div>
   );
